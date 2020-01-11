f562f5d66aeb14686ccb60f6d757734cfbe2effb
Henning Wobken <henning.wobken@simplex24.de>
2020-01-11 23:55:26 +0100

    issue-42: removed unused model

--- projects/simplemattable/src/lib/model/table-cell.model.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Class used internally to represent state associated with a table cell.
- * Do not use this class outside of simplemattable
- */
-export class TableColumn<T, P extends keyof T> {
-  tableColumn: TableColumn<T, P>;
-  onChange: () => void;
-}
